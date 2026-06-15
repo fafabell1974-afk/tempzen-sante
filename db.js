e58764029111fdd75d69a0f03f0c163d15918f9e
fafabell1974-afk <fafa.bell1974@gmail.com>
2026-06-15 18:10:10 +0200

    Refactor database initialization and data addition

--- db.js
+++ db.js
@@ -1,6 +1,5 @@
 const DB_NAME = 'TempZenDB';
 let db;
-
 const initDB = () => {
     return new Promise((resolve) => {
         const request = indexedDB.open(DB_NAME, 3);
@@ -8,20 +7,17 @@ const initDB = () => {
             db = e.target.result;
             if (!db.objectStoreNames.contains('medicaments')) db.createObjectStore('medicaments', { keyPath: 'id', autoIncrement: true });
             if (!db.objectStoreNames.contains('suivi')) db.createObjectStore('suivi', { keyPath: 'id', autoIncrement: true });
-            if (!db.objectStoreNames.contains('rdv')) db.createObjectStore('rdv', { keyPath: 'id', autoIncrement: true });
         };
-        request.onsuccess = (e) => { 
-            db = e.target.result; 
-            resolve();
-        };
+        request.onsuccess = (e) => { db = e.target.result; resolve(); };
     });
 };
-
 function addData(store, data) {
-    const tx = db.transaction(store, 'readwrite');
-    tx.objectStore(store).add(data);
+    return new Promise(resolve => {
+        const tx = db.transaction(store, 'readwrite');
+        tx.objectStore(store).add(data);
+        tx.oncomplete = () => resolve();
+    });
 }
-
 function getAll(store) {
     return new Promise(resolve => {
         const tx = db.transaction(store, 'readonly');
