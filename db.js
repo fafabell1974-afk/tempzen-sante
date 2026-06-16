53808d2db891f8aafd67f440ee25473e889647c8
fafabell1974-afk <fafa.bell1974@gmail.com>
2026-06-16 19:55:44 +0200

    Update db.js

--- db.js
+++ db.js
@@ -1,9 +1,8 @@
 const DB_NAME = 'TempZenDB';
 let db;
-
 const initDB = () => {
     return new Promise((resolve) => {
-        const request = indexedDB.open(DB_NAME, 3);
+        const request = indexedDB.open(DB_NAME, 4);
         request.onupgradeneeded = (e) => {
             db = e.target.result;
             if (!db.objectStoreNames.contains('medicaments')) db.createObjectStore('medicaments', { keyPath: 'id', autoIncrement: true });
@@ -12,27 +11,6 @@ const initDB = () => {
         request.onsuccess = (e) => { db = e.target.result; resolve(); };
     });
 };
-
-function addData(store, data) {
-    return new Promise(resolve => {
-        const tx = db.transaction(store, 'readwrite');
-        tx.objectStore(store).add(data);
-        tx.oncomplete = () => resolve();
-    });
-}
-
-function getAll(store) {
-    return new Promise(resolve => {
-        const tx = db.transaction(store, 'readonly');
-        const req = tx.objectStore(store).getAll();
-        req.onsuccess = () => resolve(req.result);
-    });
-}
-
-function deleteData(store, id) {
-    return new Promise(resolve => {
-        const tx = db.transaction(store, 'readwrite');
-        tx.objectStore(store).delete(id);
-        tx.oncomplete = () => resolve();
-    });
-}
+function addData(store, data) { return new Promise(resolve => { const tx = db.transaction(store, 'readwrite'); tx.objectStore(store).add(data); tx.oncomplete = () => resolve(); }); }
+function getAll(store) { return new Promise(resolve => { const tx = db.transaction(store, 'readonly'); const req = tx.objectStore(store).getAll(); req.onsuccess = () => resolve(req.result); }); }
+function deleteData(store, id) { return new Promise(resolve => { const tx = db.transaction(store, 'readwrite'); tx.objectStore(store).delete(id); tx.oncomplete = () => resolve(); }); }
