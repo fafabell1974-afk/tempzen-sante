2cc5d0af3ce4e0ce9c32cce1770c50a14e3825d5
fafabell1974-afk <fafa.bell1974@gmail.com>
2026-06-16 19:31:30 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,5 @@
-const CACHE = 'tempzen-offline-v1';
+const CACHE = 'tempzen-offline-v2'; // CHANGEMENT ICI (v1 -> v2)
+
 self.addEventListener('install', (e) => {
     e.waitUntil(caches.open(CACHE).then(c => c.addAll([
         '/', 
@@ -9,6 +10,19 @@ self.addEventListener('install', (e) => {
         'icon-192.png', 
         'icon-512.png'
     ])));
+});
+
+// Ajout d'une étape pour supprimer l'ancien cache lors de l'activation
+self.addEventListener('activate', (e) => {
+    e.waitUntil(
+        caches.keys().then((keyList) => {
+            return Promise.all(keyList.map((key) => {
+                if (key !== CACHE) {
+                    return caches.delete(key);
+                }
+            }));
+        })
+    );
 });
 
 self.addEventListener('fetch', (e) => {
