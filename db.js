724dc24342b758aa11a2bae41fe87c7c9cc9741e
fafabell1974-afk <fafa.bell1974@gmail.com>
2026-06-16 19:49:27 +0200

    Update db.js

--- db.js
+++ db.js
@@ -1,5 +1,6 @@
 const DB_NAME = 'TempZenDB';
 let db;
+
 const initDB = () => {
     return new Promise((resolve) => {
         const request = indexedDB.open(DB_NAME, 3);
@@ -11,6 +12,7 @@ const initDB = () => {
         request.onsuccess = (e) => { db = e.target.result; resolve(); };
     });
 };
+
 function addData(store, data) {
     return new Promise(resolve => {
         const tx = db.transaction(store, 'readwrite');
@@ -18,10 +20,19 @@ function addData(store, data) {
         tx.oncomplete = () => resolve();
     });
 }
+
 function getAll(store) {
     return new Promise(resolve => {
         const tx = db.transaction(store, 'readonly');
         const req = tx.objectStore(store).getAll();
         req.onsuccess = () => resolve(req.result);
+    });
+}
+
+function deleteData(store, id) {
+    return new Promise(resolve => {
+        const tx = db.transaction(store, 'readwrite');
+        tx.objectStore(store).delete(id);
+        tx.oncomplete = () => resolve();
     });
 }
