6f18e0710dcb6fff3349044e5dcf586d67828105
fafabell1974-afk <fafa.bell1974@gmail.com>
2026-06-16 19:51:02 +0200

    Update cache files in service worker
    Removed 'styles.css' from the cache during the install event.

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 const CACHE = 'tempzen-offline-v2';
 self.addEventListener('install', (e) => {
-    e.waitUntil(caches.open(CACHE).then(c => c.addAll(['/', 'index.html', 'styles.css', 'app.js', 'db.js'])));
+    e.waitUntil(caches.open(CACHE).then(c => c.addAll(['/', 'index.html', 'app.js', 'db.js'])));
 });
 self.addEventListener('activate', (e) => {
     e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => k !== CACHE ? caches.delete(k) : null))));
