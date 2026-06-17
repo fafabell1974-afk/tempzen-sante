c369927609a08d4beeca1f82b8078de9a6ae667f
fafabell1974-afk <fafa.bell1974@gmail.com>
2026-06-17 19:29:44 +0200

    Update and rename service-worker.js to sw.js

---
+++ sw.js
@@ -0,0 +1,10 @@
+const CACHE = 'tempzen-offline-v2';
+self.addEventListener('install', (e) => {
+    e.waitUntil(caches.open(CACHE).then(c => c.addAll(['/', 'index.html', 'app.js', 'db.js'])));
+});
+self.addEventListener('activate', (e) => {
+    e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => k !== CACHE ? caches.delete(k) : null))));
+});
+self.addEventListener('fetch', (e) => {
+    e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+});
