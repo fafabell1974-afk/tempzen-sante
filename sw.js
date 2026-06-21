986e54a86ca29b7cd81a5a5fef57b50c436ea16b
fafabell1974-afk <fafa.bell1974@gmail.com>
2026-06-21 09:08:41 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,50 +1,42 @@
-const CACHE = "tempzen-v1";
-
+const CACHE = "tempzen-v2";
 
 const files = [
-
-"./",
-"./index.html",
-"./styles.css",
-"./app.js",
-"./db.js",
-"./manifest.json",
-
-"./icon-192.png",
-"./icon-512.png"
-
+  "./",
+  "./index.html",
+  "./styles.css",
+  "./app.js",
+  "./db.js",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
 ];
 
-
-
-
-self.addEventListener("install",e=>{
-
-
-e.waitUntil(
-
-caches.open(CACHE)
-.then(cache=>cache.addAll(files))
-
-);
-
-
+self.addEventListener("install", e => {
+  self.skipWaiting();
+  e.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(files))
+  );
 });
 
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(key => key !== CACHE)
+            .map(key => caches.delete(key))
+      )
+    ).then(() => self.clients.claim())
+  );
+});
 
-
-
-
-
-self.addEventListener("fetch",e=>{
-
-
-e.respondWith(
-
-caches.match(e.request)
-.then(r=>r || fetch(e.request))
-
-);
-
-
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    fetch(e.request)
+      .then(response => {
+        let copy = response.clone();
+        caches.open(CACHE).then(cache => cache.put(e.request, copy));
+        return response;
+      })
+      .catch(() => caches.match(e.request))
+  );
 });
