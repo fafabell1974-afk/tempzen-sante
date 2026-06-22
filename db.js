07d93d22de4f55a7f08d62f1abb82d282cd50e62
fafabell1974-afk <fafa.bell1974@gmail.com>
2026-06-22 16:42:15 +0200

    Refactor database initialization and CRUD operations

--- db.js
+++ db.js
@@ -1,106 +1,173 @@
 let db;
 
+
 function initDB(){
-  return new Promise((resolve,reject)=>{
 
-    const request = indexedDB.open("TempZenDB",7);
-
-    request.onupgradeneeded = function(event){
-      db = event.target.result;
-
-      if(!db.objectStoreNames.contains("medicaments")){
-        db.createObjectStore("medicaments",{keyPath:"id",autoIncrement:true});
-      }
-
-      if(!db.objectStoreNames.contains("rdv")){
-        db.createObjectStore("rdv",{keyPath:"id",autoIncrement:true});
-      }
-
-      if(!db.objectStoreNames.contains("suivi")){
-        db.createObjectStore("suivi",{keyPath:"id",autoIncrement:true});
-      }
-
-      if(!db.objectStoreNames.contains("contacts")){
-        db.createObjectStore("contacts",{keyPath:"id",autoIncrement:true});
-      }
-
-      if(!db.objectStoreNames.contains("ordonnances")){
-        db.createObjectStore("ordonnances",{keyPath:"id",autoIncrement:true});
-      }
-
-    };
+return new Promise((resolve,reject)=>{
 
 
-    request.onsuccess = function(event){
-      db = event.target.result;
-      resolve();
-    };
+const request = indexedDB.open("TempZenDB",8);
 
 
-    request.onerror = function(event){
-      console.log("Erreur DB", event.target.error);
-      reject(event.target.error);
-    };
 
-  });
+request.onupgradeneeded=function(event){
+
+db=event.target.result;
+
+
+
+if(!db.objectStoreNames.contains("medicaments")){
+db.createObjectStore("medicaments",{keyPath:"id",autoIncrement:true});
 }
+
+
+if(!db.objectStoreNames.contains("rdv")){
+db.createObjectStore("rdv",{keyPath:"id",autoIncrement:true});
+}
+
+
+if(!db.objectStoreNames.contains("suivi")){
+db.createObjectStore("suivi",{keyPath:"id",autoIncrement:true});
+}
+
+
+if(!db.objectStoreNames.contains("contacts")){
+db.createObjectStore("contacts",{keyPath:"id",autoIncrement:true});
+}
+
+
+if(!db.objectStoreNames.contains("ordonnances")){
+
+db.createObjectStore("ordonnances",{
+keyPath:"id",
+autoIncrement:true
+});
+
+}
+
+
+};
+
+
+
+request.onsuccess=function(event){
+
+db=event.target.result;
+resolve();
+
+};
+
+
+
+request.onerror=function(event){
+
+reject(event.target.error);
+
+};
+
+
+});
+
+}
+
+
+
+
 
 
 
 function addData(store,data){
-  return new Promise((resolve,reject)=>{
 
-    let transaction = db.transaction(store,"readwrite");
-    let object = transaction.objectStore(store);
+return new Promise((resolve,reject)=>{
 
-    object.add(data);
 
-    transaction.oncomplete = () => resolve();
-    transaction.onerror = (e) => reject(e.target.error);
+let transaction=db.transaction(store,"readwrite");
 
-  });
+let object=transaction.objectStore(store);
+
+
+let req=object.add(data);
+
+
+
+req.onerror=function(e){
+
+reject(e.target.error);
+
+};
+
+
+
+transaction.oncomplete=function(){
+
+resolve();
+
+};
+
+
+});
+
 }
+
+
+
+
 
 
 
 function getData(store){
-  return new Promise((resolve,reject)=>{
 
-    let transaction = db.transaction(store,"readonly");
-    let request = transaction.objectStore(store).getAll();
+return new Promise((resolve,reject)=>{
 
-    request.onsuccess = () => resolve(request.result);
-    request.onerror = (e) => reject(e.target.error);
 
-  });
+let transaction=db.transaction(store,"readonly");
+
+
+let request=transaction.objectStore(store).getAll();
+
+
+request.onsuccess=function(){
+
+resolve(request.result);
+
+};
+
+
+request.onerror=function(e){
+
+reject(e.target.error);
+
+};
+
+
+});
+
 }
 
 
 
-function updateData(store,data){
-  return new Promise((resolve,reject)=>{
 
-    let transaction = db.transaction(store,"readwrite");
-
-    transaction.objectStore(store).put(data);
-
-    transaction.oncomplete = () => resolve();
-    transaction.onerror = (e) => reject(e.target.error);
-
-  });
-}
 
 
 
 function deleteData(store,id){
-  return new Promise((resolve,reject)=>{
 
-    let transaction = db.transaction(store,"readwrite");
+return new Promise((resolve,reject)=>{
 
-    transaction.objectStore(store).delete(id);
 
-    transaction.oncomplete = () => resolve();
-    transaction.onerror = (e) => reject(e.target.error);
+let transaction=db.transaction(store,"readwrite");
 
-  });
+
+transaction.objectStore(store).delete(id);
+
+
+
+transaction.oncomplete=()=>resolve();
+
+
+transaction.onerror=e=>reject(e.target.error);
+
+
+});
+
 }
